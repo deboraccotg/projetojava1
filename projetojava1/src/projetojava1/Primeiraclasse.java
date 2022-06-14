3be36d3e1a9d58ec17aaf048b0c36f45c3652f3f
DEBORA <DEBORA@DEBORA-PC>
2022-06-14 11:46:34 -0300

    teste

---
+++ projetojava1/src/projetojava1/Primeiraclasse.java
@@ -0,0 +1,15 @@
+package projetojava1;
+import java.util.Scanner;
+
+public class Primeiraclasse {
+
+	public static void main(String[] args) {
+		int a, b, c = 0;
+	      Scanner s = new Scanner( System.in );
+	      System.out.println( "Introduce dos números: ");
+	      a = s.nextInt();
+	      b = s.nextInt();
+	      c = a + b;
+	      System.out.println( "La suma de a y b es: " + c );
+}
+}
